a08e1abbf01a822b24dc9fbde959f81ebd0c3c57
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-08-09 09:52:15 +0545

    tools setup

--- src/agent/tools.ts
+++ src/agent/tools.ts
@@ -1,7 +1,14 @@
 import { TavilySearch } from "@langchain/tavily";
+import { SERPGoogleScholarAPITool } from "@langchain/community/tools/google_scholar";
 
 export const tavilySearch = new TavilySearch({
   tavilyApiKey: process.env.TAVILY_API_KEY!,
   maxResults: 1,
   includeRawContent: false,
 });
+
+export const googleScholar = new SERPGoogleScholarAPITool({
+  apiKey: process.env.SERPAPI_API_KEY!,
+});
+
+export const tools = [tavilySearch, googleScholar];
